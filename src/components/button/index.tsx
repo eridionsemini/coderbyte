d1223dc6ee34b41f952eb8b52b9eafccd09d4f6b
eridion semini <eridionsemini@gmail.com>
2023-04-24 14:44:18 +0200

    added hitslop

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import {TouchableOpacity, Text} from 'react-native';
 import styles from './styles';
 
+const hitSlop = {
+  left: 10,
+  right: 10,
+  top: 20,
+  bottom: 20,
+};
+
 const Button: React.FC<{
   title: string;
   disabled: boolean;
@@ -9,6 +16,7 @@ const Button: React.FC<{
 }> = ({title, disabled, onPress}) => {
   return (
     <TouchableOpacity
+      hitSlop={hitSlop}
       disabled={disabled}
       onPress={onPress}
       style={disabled ? styles.buttonDisabled : styles.button}>
